084bab8bbe7f424c88975196d2a44deac1117d01
Yuhang Xu <xuyuhang1005@gmail.com>
2014-01-28 20:17:54 -0800

    Add static server.

--- app.js
+++ app.js
@@ -1,36 +1,16 @@
 
-/**
- * Module dependencies.
- */
+var static = require('node-static');
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
+//
+// Create a node-static server instance to serve the './app' folder
+//
+var file = new(static.Server)('./app');
+var port = process.env.PORT || 5000;
 
-var app = express();
+require('http').createServer(function (request, response) {
+  request.addListener('end', function () {
+    file.serve(request, response);
+  });
+}).listen(port);
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+console.log('Listening on port ' + port);
